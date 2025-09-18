53f7451fba39d431f05d06935d72235373c0d3f2
Dobbymin <gmin990624@gmail.com>
2025-09-18 13:50:55 +0900

    chore: 쉘 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-cd ../
+
+# 스크립트 실행 중 오류 발생 시 즉시 중단
+set -e
+
+# 이전 output 디렉토리가 있다면 삭제하고 새로 생성
+rm -rf output
 mkdir output
-cp -R ./ClipFE-v2/* ./output
-cp -R ./output ./ClipFE-v2/
+
+git archive HEAD | tar -x -C output
